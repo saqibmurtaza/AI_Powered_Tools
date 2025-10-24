c2761b00151ec38f4bf3c21842fba54cc4bd58a6
saqibmurtaza <saqibmurtazakhan@gmai.com>
2025-10-24 14:58:37 +0500

    Context Card Tool CSP issue during deployment

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,21 @@
-import type { NextConfig } from "next";
+// frontend/next.config.ts
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "script-src 'self' 'unsafe-eval' 'unsafe-inline' https: http:;"
+          }
+        ],
+      },
+    ];
+  },
+}
 
-export default nextConfig;
+export default nextConfig
+
